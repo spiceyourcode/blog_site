e241b4c3eaa84c77cb8f734ea073e8af94c08761
frankline <130725052+spiceyourcode@users.noreply.github.com>
2025-03-29 23:16:31 +0300

    `Added post_list view in blog/views.py and updated mysite/urls.py to include blog app URLs.`

--- omari.py
+++
@@ -1,8 +0,0 @@
-import zoneinfo 
-time=zoneinfo.available_timezones()
-
-
-with open('timezones.txt', 'w') as file:
-    time_in_srting=str(time)
-    file.write(time_in_srting)
-    print("operatiion done successfully")
